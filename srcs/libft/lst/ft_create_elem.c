96d9e7edb3d8733fc5977c9ef7cb6b69e24ef2c1
Julien DUMAY <judumay@student.42.fr>
2019-05-22 15:48:02 +0200

    Adding some new functions

--- srcs/libft/lst/ft_create_elem.c
+++ srcs/libft/lst/ft_create_elem.c
@@ -6,21 +6,19 @@
 /*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:26:45 by judumay           #+#    #+#             */
-/*   Updated: 2019/02/25 13:56:44 by judumay          ###   ########.fr       */
+/*   Updated: 2019/05/22 15:47:31 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 t_list	*ft_create_elem(void *data)
 {
-	t_list	*elem;
+	t_list	*new;
 
-	elem = malloc(sizeof(t_list));
-	if (elem)
-	{
-		elem->content = data;
-		elem->next = NULL;
-	}
-	return (elem);
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new->next = NULL;
+	new->data = data;
+	return (new);
 }
